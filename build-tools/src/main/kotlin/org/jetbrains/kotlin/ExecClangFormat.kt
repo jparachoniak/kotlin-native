fb185b00b0350d54cc81ae0070921c4592d2fb67
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-03-06 11:45:08 +0300

    Add .clang-format based on Chromium style and enable it in runtime/

---
+++ build-tools/src/main/kotlin/org/jetbrains/kotlin/ExecClangFormat.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.kotlin
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+
+class ExecClangFormat(private val project: Project) {
+    private fun executable(): String {
+        val llvmDir = project.findProperty("llvmDir")
+        return "$llvmDir/bin/clang-format"
+    }
+
+    fun fix(target: String) {
+        project.exec {
+            it.executable = executable()
+            it.args = listOf("-i", target)
+        }
+    }
+
+    fun check(target: String) {
+        val outputStream = ByteArrayOutputStream()
+        project.exec {
+            it.executable = executable()
+            it.args = listOf(target)
+            it.standardOutput = outputStream
+        }.assertNormalExitValue()
+        val diffOutputStream = ByteArrayOutputStream()
+        val diffResult = project.exec {
+            it.commandLine = listOf("diff", "-u", target, "-")
+            it.standardInput = ByteArrayInputStream(outputStream.toByteArray())
+            it.standardOutput = diffOutputStream
+            it.isIgnoreExitValue = true
+        }
+        if (diffResult.exitValue == 0) {
+            return
+        }
+        throw GradleException("$target is not formatted:\n$diffOutputStream")
+    }
+}
